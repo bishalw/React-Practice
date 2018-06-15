cbd000b7530f66ec7e51d1df7adaee19655e2c70
Bishal Wagle <bishalw@gmail.com>
2018-06-14 20:22:14 -0700

    initial commit

---
+++ ReactHelloWorld/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function(){
+  return <div>Hello World</div>;
+}
+ReactDOM.render(<App />, document.querySelector('.container') );
